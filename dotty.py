47884ba93f3836effd30495e484c467cdd344196
Vibhav Pant <vibhavp@gmail.com>
2015-02-26 12:56:55 +0530

    Initial Commit.

---
+++ dotty.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import json
+import sys
+import os
+
+def main():
+    try:
+        js = json.load(open(sys.argv[1]))
+    except Exception as e:
+        print("Usage: dotty.py FILE", file=sys.stderr)
+        exit(1)
+
+    directories  = js.get("directories")
+    links = js.get("links")
+    # Check if directories exist.
+    for path in directories:
+        exp = os.path.expanduser(path)
+        if (not os.path.isdir(exp)):
+            print(exp+" doesnt exist, creating.")
+            os.makedirs(exp)
+
+    for src in links:
+        dest = os.path.expanduser(links[src])
+        src = os.path.abspath(src)
+        print("Linking %s -> %s" % (dest, src))
+        os.symlink(src, dest)
+
+if __name__ == "__main__":
+    main()
